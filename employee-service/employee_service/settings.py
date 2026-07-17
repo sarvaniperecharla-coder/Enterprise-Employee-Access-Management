f3d35bd26f3f0195eb62bd8ead489efc5b70e942
Sarvani Perecharla <sarvaniperecharla@gmail.com>
2026-07-17 13:19:07 +0530

    feat: implement pagination for Employee API

--- employee-service/employee_service/settings.py
+++ employee-service/employee_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'employees',
 ]
 
@@ -126,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+}
--- employee-service/employees/views.py
+++ employee-service/employees/views.py
@@ -4,5 +4,33 @@ from .serializers import EmployeeSerializer
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    CRUD API for Employee
+    """
+
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    # Search functionality
+    search_fields = [
+        "first_name",
+        "last_name",
+        "email",
+        "designation",
+    ]
+
+    # Filter functionality
+    filterset_fields = [
+        "designation",
+        "is_active",
+    ]
+
+    # Ordering functionality
+    ordering_fields = [
+        "salary",
+        "date_of_joining",
+        "first_name",
+    ]
+
+    # Default ordering
+    ordering = ["first_name"]
