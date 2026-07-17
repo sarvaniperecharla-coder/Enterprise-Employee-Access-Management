f1fee9e240115911cfda8d146b368f779fa8dcb0
Sarvani Perecharla <sarvaniperecharla@gmail.com>
2026-07-17 12:53:28 +0530

    feat: customize Django admin for employee management

--- employee-service/employees/admin.py
+++ employee-service/employees/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "designation",
+        "salary",
+        "is_active",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+
+    list_filter = (
+        "designation",
+        "is_active",
+    )
+
+    ordering = ("first_name",)
